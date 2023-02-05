708a3e61c1e0d33e590506836cb089658069d991
doctor <doctor192322@gmail.com>
2023-02-05 23:43:27 +0800

    update chat

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { NavigationContainer, DarkTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Frist from './frist';
-import SendIcon from './assets/icon_sendmessage.svg';
+import MoreIcon from './assets/icon_more.svg';
+import SmileIcon from './assets/icon_smile.svg';
+import VoiceIcon from './assets/icon_voice.svg';
 import DoneIcon from './assets/icon_doneall.svg';
 import SendScreen from './pages/wallet/send';
 import TradeScreen from './pages/wallet/trade';
@@ -34,8 +36,21 @@ function ProfileScreen() {
 function AccountScreen() {
   return <View />;
 }
+const UselessTextInput = () => {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
+  return (
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', color: '#fff', flex: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+  );
+}
 
 function SettingsScreen() {
+  const [value, onChangeText] = React.useState('33');
+
   return <View>
     <ScrollView
       contentContainerStyle={{ minHeight: '85%' }}
@@ -71,9 +86,9 @@ function SettingsScreen() {
             source={require('./assets/ks.jpg')}
           />
         </View>
-        <View>
-          <View style={{ padding: 10, backgroundColor: 'rgba(255,255,255,0.1)',  marginLeft: 20, borderRadius: 100, borderBottomLeftRadius: 0 }}>
-            <Text style={{ color: '#fff',flex:1 }}>ok</Text>
+        <View style={{ marginLeft: 20, }}>
+          <View style={{ padding: 10, backgroundColor: 'rgba(255,255,255,0.1)', borderRadius: 100, borderBottomLeftRadius: 0, width: 40 }}>
+            <Text style={{ color: '#fff', flex: 1 }}>ok</Text>
 
           </View>
           <Text style={{ padding: 10, color: 'rgba(255,255,255,0.3)', fontSize: 12, textAlign: 'center', marginLeft: 20, }}>17:01</Text>
@@ -92,7 +107,7 @@ function SettingsScreen() {
           <View style={{ padding: 10, backgroundColor: '#422DDD', marginLeft: 5, borderRadius: 100, borderBottomRightRadius: 0 }}>
             <Text style={{ color: '#fff' }}>A string representing the </Text>
           </View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center',marginTop:10 }}>
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
             <View style={{ display: 'flex', flexDirection: 'row' }}>
               <Text style={{ paddingLeft: 10, color: 'rgba(255,255,255,0.3)', fontSize: 12 }}>18:22</Text>
               <DoneIcon width={20} height={20} fill="rgba(255,255,255,0.3)" />
@@ -134,7 +149,7 @@ function SettingsScreen() {
           <View style={{ padding: 10, backgroundColor: '#422DDD', marginLeft: 5, borderRadius: 10, borderBottomRightRadius: 0 }}>
             <Text style={{ color: '#fff' }}>An accessibility hint helps users understand what will happen when they perform an action on the accessibility element when that result is not obvious from the accessibility label.</Text>
           </View>
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center',marginTop:10 }}>
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
             <View style={{ display: 'flex', flexDirection: 'row' }}>
               <Text style={{ paddingLeft: 10, color: 'rgba(255,255,255,0.3)', fontSize: 12 }}>18:22</Text>
               <DoneIcon width={20} height={20} fill="rgba(255,255,255,0.3)" />
@@ -143,8 +158,8 @@ function SettingsScreen() {
         </View>
 
       </View>
-     
-     
+
+
       <View style={{ display: 'flex', flexDirection: 'row', marginBottom: 20 }}>
         <View>
           <Image
@@ -203,9 +218,32 @@ function SettingsScreen() {
         </View>
       </View> */}
     </ScrollView>
-    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', borderTopWidth: 1, borderTopColor: 'rgba(255,255,255,0.1)', paddingLeft: 10, paddingRight: 10 }}>
-      <TextInput value='Send Message' style={{ color: '#fff', flex: 1 }} />
-      <SendIcon width={30} height={30} fill={'white'} />
+    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', borderTopColor: 'rgba(255,255,255,0.1)', paddingLeft: 10, paddingRight: 10 }}>
+      <VoiceIcon width={30} height={30} fill="rgba(255,255,255,0.7)" />
+      <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1, padding: 10, borderRadius: 100, backgroundColor: 'rgba(255,255,255,0.05)', height: 40, marginLeft: 10, marginRight: 10 }}>
+        {/* <TextInput style={{ color: 'red',flex:1 }}  
+      defaultValue="123" /> */}
+        {/* <UselessTextInput /> */}
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', color: '#fff', flex: 1 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+        <SmileIcon style={{}} width={30} height={30} fill="rgba(255,255,255,0.7)" />
+
+      </View>
+      {
+        value!=""&&<Button
+        title="Send"
+        color="#422DDD"
+        style={{texttran}}
+      />||
+      <MoreIcon width={30} height={30} fill="rgba(255,255,255,0.7)" />
+
+      }
+
+      {/*  */}
+
     </View>
   </View>
     ;
