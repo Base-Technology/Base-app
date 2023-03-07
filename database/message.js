fec589a3bd0e793776aefa801da500f2b644d5d4
bruden <lulargehead@gmail.com>
2023-03-08 00:00:51 +0800

    use promise in sqlite

--- database/message.js
+++ database/message.js
@@ -2,29 +2,32 @@ import SQLite from './sqlite';
 import uuid from 'react-native-uuid';
 import moment from 'moment';
 
-export function queryMessage(callback) {
+export async function queryMessage(callback) {
     const sqlite = SQLite.getInstance();
-    sqlite.executeSql(`SELECT * FROM "message" ORDER BY "timestamp"`, [], (tx, results) => {
-        const messages = [];
-        for (let i = 0; i < results.rows.length; i++) {
-            const message = results.rows.item(i);
-            messages.push(message);
-        }
+    const results = await sqlite.executeSql(`SELECT * FROM "message" ORDER BY "timestamp"`);
+    const messages = [];
+    for (let i = 0; i < results.rows.length; i++) {
+        const message = results.rows.item(i);
+        messages.push(message);
+    }
+    if (callback) {
         callback(messages);
-    });
+    }
+    return messages;
 }
 
-export function addMessage(message, callback) {
+export async function addMessage(message, callback) {
     const sqlite = SQLite.getInstance();
     // TODO: mock data
-    message.id = uuid.v4();;
+    message.id = uuid.v4();
     message.state = 0;
     message.timestamp = moment().valueOf();
     message.profile_id = '0x123';
     message.is_send = 1;
-    sqlite.executeSql(`INSERT into "message" VALUES (?,?,?,?,?,?)`,
+    const results = await sqlite.executeSql(`INSERT into "message" VALUES (?,?,?,?,?,?)`,
         [message.id, message.state, message.timestamp, message.profile_id, message.is_send, message.content],
-        (tx, results) => {
-            callback(message);
-        });
+    );
+    if (callback) {
+        callback(message);
+    }
 }
