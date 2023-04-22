20f996cfef343a11927e9721cbc31f66d64c57f5
Oiching-SuiLin <13425535469@163.com>
2023-04-22 09:55:55 +0800

    refactor : add comments

--- connectFunctions/BaseLen/post.js
+++ connectFunctions/BaseLen/post.js
@@ -1,3 +1,14 @@
+/* 用于发布publication
+* sender : 地址，合约调用的发起者账户，即合约钱包的拥有者
+* wallet ： 对象，合约钱包的合约
+* baseHub ： 对象，Hub和合约
+* profileId : int，发帖者的profileid
+* contentURI ： 字符串，内容的标识
+* collectModule : 地址，用户为该publication选定的collect module
+* collectModuleData ： 初始化profile 的 collect module时传递的参数
+* referenceModule ： 地址，用户为该publication选定的reference module
+* referenceModuleData ： 初始化profile 的 reference module时传递的参数
+*/
 export async function post(
   sender,
   wallet,
